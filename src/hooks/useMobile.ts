41213fd41a888517b1c6845a3c6267d27e81ccf2
okky02 <okkydhelfilano@gmail.com>
2025-11-22 22:59:54 +0700

    hooks size mobile chat

---
+++ src/hooks/useMobile.ts
@@ -0,0 +1,21 @@
+// hooks/useMobile.ts
+import { useState, useEffect } from "react";
+
+export function useMobile(): boolean {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkDevice();
+    window.addEventListener("resize", checkDevice);
+
+    return () => {
+      window.removeEventListener("resize", checkDevice);
+    };
+  }, []);
+
+  return isMobile;
+}
